af1c487510ae97d32c4a309a092e1bac0caea367
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-02-14 12:06:51 +0100

    ♻️ F01: AUTH > refactor <Logo/>

--- src/components/AuthComponents/Logo.jsx
+++ src/components/AuthComponents/Logo.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { LogoBox, LogoImg } from '../styles/LogoBox.styled'
+import styled from "styled-components"
 
 const Logo = () => {
   return (
-  <LogoBox>
-    <LogoImg src='./src/assets/LOGO BLANC CTW.png'/>
-  </LogoBox>
+    <LogoStyled className="logo-box">
+      <img className="logo-img" src="./src/assets/LOGO BLANC CTW.png" />
+    </LogoStyled>
   )
 }
 
 export default Logo
+
+export const LogoStyled = styled.div`
+  width: ${(props) => (props.small ? "25vw" : "15vw")};
+  margin: 0 auto;
+  padding-top: ${(props) => (props.small ? "10vh" : "5vh")};
+  .logo-img {
+    width: 100%;
+  }
+
+  @media (max-width: 450px) {
+    width: 70vw;
+    padding-bottom: 2vh;
+  }
+`
